021f44b0b0e3603dcd890a65d5f8b3a88da02d26
dsetoain <130994309+dsetoain@users.noreply.github.com>
2025-09-26 19:24:32 +0200

    Delete oop-24-25-final-project-group29-master directory

--- oop-24-25-final-project-group29-master/app/Welcome.py
+++
@@ -1,8 +0,0 @@
-# from autoop.core.ml.artifact import Artifact
-import streamlit as st
-
-st.set_page_config(
-    page_title="AutoML app",
-    page_icon="👋",
-)
-st.sidebar.success("Select a page above.")
